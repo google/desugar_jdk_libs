e93c4e9da2374a793791c218f7901014bf0d1ea7
kmb <kmb@google.com>
2019-08-14 11:17:35 -0700

    add header to added files
    PiperOrigin-RevId: 263389387

--- tools/build_maven_artifact.py
+++ tools/build_maven_artifact.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
+# Copyright 2019 Google LLC
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 
 """Generate a maven library.
 """
