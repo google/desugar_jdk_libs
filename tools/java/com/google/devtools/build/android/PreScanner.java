b7ad3e296f136c096f25a5caa50a5819166644f7
sgjesse <sgjesse@google.com>
2022-02-23 07:32:03 +0000

    BEGIN_PUBLIC\nZInternal Change\nEND_PUBLIC
    PiperOrigin-RevId: 430379174

--- tools/java/com/google/devtools/build/android/PreScanner.java
+++ tools/java/com/google/devtools/build/android/PreScanner.java
@@ -187,7 +187,7 @@ public final class PreScanner {
       }
     }
 
-    // TODO(b/207007439): Move to google3/third_party/java/r8/full_desugar_jdk_libs.json
+    // TODO(b/207007439): Move to full_desugar_jdk_libs.json configuration
     // once the proper Android SDK levels have been accessed.
     typeReplacements.put("jdk/internal/misc/Unsafe", "sun/misc/DesugarUnsafe");
     typeReplacements.put(
