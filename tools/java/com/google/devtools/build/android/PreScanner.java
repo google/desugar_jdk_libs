b0314979f9d1ee632ac7ade1559a320967fb70b9
Donald Duo Zhao <deltazulu@google.com>
2022-08-25 11:13:12 -0700

    Automated rollback of commit 292df0eea1c2c1d6b8fe834c7b347ef0b0fdc11b.
    *** Reason for rollback ***
    
    Break CHM Build:
    
    Repo:
    ```
    blaze build  --config=android_fix_libs   //javatests/com/google/android/apps/userpanel/activities:MainActivityTest_generic_phone_21_x86_en-US
    ```
    
    [Failed Waymo Target]([]
    
    *** Original change description ***
    
    Move Pre-Pass `desugar_jdk_libs` Retargeting to D8-desugar-based - `jdk.misc.internal.Unsafe`
    
    PiperOrigin-RevId: 470035869

--- tools/java/com/google/devtools/build/android/PreScanner.java
+++ tools/java/com/google/devtools/build/android/PreScanner.java
@@ -186,6 +186,10 @@ public final class PreScanner {
             AsmHelpers.getReplacementTypeInternalName(anonymousClassInternalName));
       }
     }
+
+    // TODO(b/207007439): Move to full_desugar_jdk_libs.json configuration
+    // once the proper Android SDK levels have been accessed.
+    typeReplacements.put("jdk/internal/misc/Unsafe", "sun/misc/DesugarUnsafe");
   }
 
   @Nullable
