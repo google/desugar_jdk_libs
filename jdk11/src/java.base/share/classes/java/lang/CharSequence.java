1a26ac14bdbfc791819beb814114a3e0d0eaa36a
clementbera <clementbera@google.com>
2026-05-04 04:28:41 -0700

    Stub out CharSequence.chars() and CharSequence.codePoints() default methods.
    PiperOrigin-RevId: 909949083

--- jdk11/src/java.base/share/classes/java/lang/CharSequence.java
+++ jdk11/src/java.base/share/classes/java/lang/CharSequence.java
@@ -25,14 +25,8 @@
 
 package java.lang;
 
-import java.util.NoSuchElementException;
 import java.util.Objects;
-import java.util.PrimitiveIterator;
-import java.util.Spliterator;
-import java.util.Spliterators;
-import java.util.function.IntConsumer;
 import java.util.stream.IntStream;
-import java.util.stream.StreamSupport;
 
 /**
  * A {@code CharSequence} is a readable sequence of {@code char} values. This
@@ -148,36 +142,38 @@ public interface CharSequence {
      * @since 1.8
      */
     public default IntStream chars() {
-        class CharIterator implements PrimitiveIterator.OfInt {
-            int cur = 0;
+        // FOR DESUGAR: Remove references to missing classes in unused code.
+        // class CharIterator implements PrimitiveIterator.OfInt {
+        //     int cur = 0;
 
-            public boolean hasNext() {
-                return cur < length();
-            }
+        //     public boolean hasNext() {
+        //         return cur < length();
+        //     }
 
-            public int nextInt() {
-                if (hasNext()) {
-                    return charAt(cur++);
-                } else {
-                    throw new NoSuchElementException();
-                }
-            }
+        //     public int nextInt() {
+        //         if (hasNext()) {
+        //             return charAt(cur++);
+        //         } else {
+        //             throw new NoSuchElementException();
+        //         }
+        //     }
 
-            @Override
-            public void forEachRemaining(IntConsumer block) {
-                for (; cur < length(); cur++) {
-                    block.accept(charAt(cur));
-                }
-            }
-        }
+        //     @Override
+        //     public void forEachRemaining(IntConsumer block) {
+        //         for (; cur < length(); cur++) {
+        //             block.accept(charAt(cur));
+        //         }
+        //     }
+        // }
 
-        return StreamSupport.intStream(() ->
-                Spliterators.spliterator(
-                        new CharIterator(),
-                        length(),
-                        Spliterator.ORDERED),
-                Spliterator.SUBSIZED | Spliterator.SIZED | Spliterator.ORDERED,
-                false);
+        // return StreamSupport.intStream(() ->
+        //         Spliterators.spliterator(
+        //                 new CharIterator(),
+        //                 length(),
+        //                 Spliterator.ORDERED),
+        //         Spliterator.SUBSIZED | Spliterator.SIZED | Spliterator.ORDERED,
+        //         false);
+        throw new UnsupportedOperationException("chars");
     }
 
     /**
@@ -198,61 +194,63 @@ public interface CharSequence {
      * @since 1.8
      */
     public default IntStream codePoints() {
-        class CodePointIterator implements PrimitiveIterator.OfInt {
-            int cur = 0;
+        // FOR DESUGAR: Remove references to missing classes in unused code.
+        // class CodePointIterator implements PrimitiveIterator.OfInt {
+        //     int cur = 0;
 
-            @Override
-            public void forEachRemaining(IntConsumer block) {
-                final int length = length();
-                int i = cur;
-                try {
-                    while (i < length) {
-                        char c1 = charAt(i++);
-                        if (!Character.isHighSurrogate(c1) || i >= length) {
-                            block.accept(c1);
-                        } else {
-                            char c2 = charAt(i);
-                            if (Character.isLowSurrogate(c2)) {
-                                i++;
-                                block.accept(Character.toCodePoint(c1, c2));
-                            } else {
-                                block.accept(c1);
-                            }
-                        }
-                    }
-                } finally {
-                    cur = i;
-                }
-            }
+        //     @Override
+        //     public void forEachRemaining(IntConsumer block) {
+        //         final int length = length();
+        //         int i = cur;
+        //         try {
+        //             while (i < length) {
+        //                 char c1 = charAt(i++);
+        //                 if (!Character.isHighSurrogate(c1) || i >= length) {
+        //                     block.accept(c1);
+        //                 } else {
+        //                     char c2 = charAt(i);
+        //                     if (Character.isLowSurrogate(c2)) {
+        //                         i++;
+        //                         block.accept(Character.toCodePoint(c1, c2));
+        //                     } else {
+        //                         block.accept(c1);
+        //                     }
+        //                 }
+        //             }
+        //         } finally {
+        //             cur = i;
+        //         }
+        //     }
 
-            public boolean hasNext() {
-                return cur < length();
-            }
+        //     public boolean hasNext() {
+        //         return cur < length();
+        //     }
 
-            public int nextInt() {
-                final int length = length();
+        //     public int nextInt() {
+        //         final int length = length();
 
-                if (cur >= length) {
-                    throw new NoSuchElementException();
-                }
-                char c1 = charAt(cur++);
-                if (Character.isHighSurrogate(c1) && cur < length) {
-                    char c2 = charAt(cur);
-                    if (Character.isLowSurrogate(c2)) {
-                        cur++;
-                        return Character.toCodePoint(c1, c2);
-                    }
-                }
-                return c1;
-            }
-        }
+        //         if (cur >= length) {
+        //             throw new NoSuchElementException();
+        //         }
+        //         char c1 = charAt(cur++);
+        //         if (Character.isHighSurrogate(c1) && cur < length) {
+        //             char c2 = charAt(cur);
+        //             if (Character.isLowSurrogate(c2)) {
+        //                 cur++;
+        //                 return Character.toCodePoint(c1, c2);
+        //             }
+        //         }
+        //         return c1;
+        //     }
+        // }
 
-        return StreamSupport.intStream(() ->
-                Spliterators.spliteratorUnknownSize(
-                        new CodePointIterator(),
-                        Spliterator.ORDERED),
-                Spliterator.ORDERED,
-                false);
+        // return StreamSupport.intStream(() ->
+        //         Spliterators.spliteratorUnknownSize(
+        //                 new CodePointIterator(),
+        //                 Spliterator.ORDERED),
+        //         Spliterator.ORDERED,
+        //         false);
+        throw new UnsupportedOperationException("codePoints");
     }
 
     /**
